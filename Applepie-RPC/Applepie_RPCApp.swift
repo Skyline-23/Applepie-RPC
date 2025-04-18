c44b0fbc657ed5cf03d78ecdd99b795d29e70de2
Skyline-23 <flight@skyline23.com>
2025-04-19 01:11:17 +0900

    Initial Commit

---
+++ Applepie-RPC/Applepie_RPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Applepie_RPCApp.swift
+//  Applepie-RPC
+//
+//  Created by 김부성 on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Applepie_RPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Applepie-RPCTests/Applepie_RPCTests.swift
@@ -0,0 +1,17 @@
+//
+//  Applepie_RPCTests.swift
+//  Applepie-RPCTests
+//
+//  Created by 김부성 on 4/19/25.
+//
+
+import Testing
+@testable import Applepie_RPC
+
+struct Applepie_RPCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
